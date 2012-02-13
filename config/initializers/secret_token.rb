e6deb0ea40e38c3814e4c7122bd450497e517029
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2012-02-12 23:09:21 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votacao::Application.config.secret_token = 'dae6b4ea997f90862a78801a3470dc57a5214d51c581fcefc129d23a0604db88eea99d245b846e02eee992966f51414f0baa456608cb8a3c52c4a5c9bd65606e'
